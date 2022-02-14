abfa9c59db1e5517663fbe9d33392a70f1f9e533
hamitmizrak <hamitmizrak@gmail.com>
2022-02-15 02:39:45 +0300

    modelmapper @Bean olarak eklemek

--- src/main/java/com/hamitmizrak/BlogReactSpringApplication.java
+++ src/main/java/com/hamitmizrak/BlogReactSpringApplication.java
@@ -1,7 +1,9 @@
 package com.hamitmizrak;
 
+import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class BlogReactSpringApplication {
@@ -10,4 +12,9 @@ public class BlogReactSpringApplication {
         SpringApplication.run(BlogReactSpringApplication.class, args);
     }
 
+
+    @Bean
+    public ModelMapper modelMapper(){
+        return new ModelMapper();
+    }
 }

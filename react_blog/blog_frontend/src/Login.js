9f5f7a0f457ce1f1440dc605e2690b989bf0fde5
hamitmizrak <hamitmizrak@gmail.com>
2022-02-14 23:32:17 +0300

    Google Chrome Component ==> input verilerini almam gerekiyor

--- react_blog/blog_frontend/src/Login.js
+++ react_blog/blog_frontend/src/Login.js
@@ -1,6 +1,37 @@
 import React from "react";
 
 class Login extends React.Component {
+  state = {
+    username: null,
+    emailaddres: null,
+    password: null,
+    repassword: null,
+  };
+
+  onChangeUsername = (event) => {
+    this.setState({
+      username: event.target.value,
+    });
+  };
+
+
+  onChangeEmail = (event) => {
+    this.setState({
+        emailaddres: event.target.value,
+    });
+  };
+
+  onChangePassword = (event) => {
+    this.setState({
+      password: event.target.value,
+    });
+  };
+
+  onChangeRepassword=(event)=>{
+      this.setState({
+          repassword:event.target.value
+      })
+  }
 
   render() {
     return (
@@ -8,19 +39,19 @@ class Login extends React.Component {
         <h1>Login</h1>
         <div>
           <label>User name</label>
-          <input />
+          <input  onChange={this.onChangeUsername} />
         </div>
         <div>
           <label>Email Address</label>
-          <input />
+          <input  onChange={this.onChangeEmail} />
         </div>
         <div>
           <label>Password</label>
-          <input type="password" />
+          <input  type="password" onChange={this.onChangePassword} />
         </div>
         <div>
           <label>Re-Password</label>
-          <input type="password" />
+          <input  type="password" onChange={this.onChangeRepassword}/>
         </div>
         <div>
           <button>Sign </button>

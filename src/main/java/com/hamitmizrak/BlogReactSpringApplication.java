cd5c2c58113d801a24228ff805655e824fb29cda
hamitmizrak <hamitmizrak@gmail.com>
2022-02-14 19:47:02 +0300

    Initial commit

---
+++ src/main/java/com/hamitmizrak/BlogReactSpringApplication.java
@@ -0,0 +1,13 @@
+package com.hamitmizrak;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlogReactSpringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BlogReactSpringApplication.class, args);
+    }
+
+}

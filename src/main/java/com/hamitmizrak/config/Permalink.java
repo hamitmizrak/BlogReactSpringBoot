e32784e56d9cfe41671f2830e5e4d10cfbf44cf0
hamitmizrak <hamitmizrak@gmail.com>
2022-02-15 11:50:04 +0300

    permalink ==> React,H2-Db,GitHub,Swagger-ui

--- src/main/java/com/hamitmizrak/config/Permalink.java
+++ src/main/java/com/hamitmizrak/config/Permalink.java
@@ -1,8 +1,16 @@
 package com.hamitmizrak.config;
 
 public class Permalink {
+
+    //React
+    //http://localhost:3000/
+
+    //H2-Db
+    //http://localhost:8080/h2-console/
+
+    //GitHub
     // https://github.com/hamitmizrak/BlogReactSpringBoot
 
     //Swagger-ui
-   // http://localhost:8080/swagger-ui/index.html
+    // http://localhost:8080/swagger-ui/index.html
 }

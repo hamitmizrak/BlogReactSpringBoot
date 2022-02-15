d496d8b1777df2940cd6f4daa01f3247cc3287cd
hamitmizrak <hamitmizrak@gmail.com>
2022-02-15 11:45:28 +0300

    permalink swagger-ui

--- src/main/java/com/hamitmizrak/config/Permalink.java
+++ src/main/java/com/hamitmizrak/config/Permalink.java
@@ -2,4 +2,7 @@ package com.hamitmizrak.config;
 
 public class Permalink {
     // https://github.com/hamitmizrak/BlogReactSpringBoot
+
+    //Swagger-ui
+   // http://localhost:8080/swagger-ui/index.html
 }

4d3857d200668149e3f22b406bb9c7589e60551e
hamitmizrak <hamitmizrak@gmail.com>
2022-02-15 13:51:03 +0300

    react içeriğine boostrap eklemek

--- react_blog/blog_frontend/src/index.js
+++ react_blog/blog_frontend/src/index.js
@@ -4,6 +4,7 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import Login from "./Login";
+import './boostrap_data.scss'
 
 ReactDOM.render(
   <React.StrictMode>
